cd5d07203a5f6d913ccd7f54ce3badc16962b660
Michael W <walkerrepair255@yahoo.com>
2022-01-22 13:21:31 -0500

    Add project files.

---
+++ SingletonExerciseNet5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SingletonExerciseNet5
+{
+    class Program
+    {
+        public static TableServers host1List = TableServers.GetTableServers();
+        public static TableServers host2List = TableServers.GetTableServers();
+        static void Main(string[] args)
+        {
+            TableServers servers = TableServers.GetTableServers();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Host1GetNextServer();
+                Host2GetNextServer();
+            }
+        }
+        public void Host1GetNextServer()
+        {
+            Console.WriteLine($"The next server is: {host1List.GetNextServer()} ");
+        }
+        public void Host2GetNextServer()
+        {
+            Console.WriteLine($"The next server is: {host2List.GetNextServer()} ");
+        }
+    }
+}
---
+++ SingletonExerciseNet5/TableServers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExerciseNet5
+{
+    public class TableServers
+    {
+        private static readonly TableServers _instance = new TableServers();
+        private List<string> servers = new List<string>();
+        private int nextServer = 0;
+
+        private TableServers()
+        {
+            servers.Add("Tim");
+            servers.Add("Todd");
+            servers.Add("Sally");
+            servers.Add("Kristy");
+        }
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+        public string GetNextServer()
+        {
+            string output = servers[nextServer];
+
+            nextServer++;
+
+            if (nextServer >= servers.Count)
+            {
+                nextServer = 0;
+            }
+            return output;
+
+        }
+    }
+}
